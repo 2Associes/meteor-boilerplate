57677ae0a7a9631103dcd5a895c1af218d787692
Hugues Brunelle <hugues@2associes.com>
2016-07-15 20:39:33 -0400

    Add AUTOPREFIXER_OPTIONS variables

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// Define flemay:less-autoprefixer options
+AUTOPREFIXER_OPTIONS = '{ "browsers": ["Chrome 36", "iOS 7"]}';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
