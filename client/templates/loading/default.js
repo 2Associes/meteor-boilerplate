54e995e0735a3d9b38ac5a63a229235f383d886c
Hugues Brunelle <hugues@2associes.com>
2016-08-01 19:43:31 -0400

    Move variables up in default.js

--- client/templates/loading/default.js
+++ client/templates/loading/default.js
@@ -2,6 +2,9 @@
 //   return Session.get('splashLoaded') ? 'homeIndex' : 'loading';
 // });
 
+var message = '<p class="loading-message"></p>';
+var spinner = '<div class="sk-circle"><div class="sk-circle1 sk-child"></div><div class="sk-circle2 sk-child"></div><div class="sk-circle3 sk-child"></div><div class="sk-circle4 sk-child"></div><div class="sk-circle5 sk-child"></div><div class="sk-circle6 sk-child"></div><div class="sk-circle7 sk-child"></div><div class="sk-circle8 sk-child"></div><div class="sk-circle9 sk-child"></div><div class="sk-circle10 sk-child"></div><div class="sk-circle11 sk-child"></div><div class="sk-circle12 sk-child"></div></div>';
+
 Template.loadingDefault.rendered = function () {
   // launch splash
   this.loading = window.pleaseWait({
@@ -26,6 +29,3 @@ Template.loadingDefault.rendered = function () {
 Template.loadingDefault.destroyed = function () {
   this.loading.finish();
 };
-
-var message = '<p class="loading-message"></p>';
-var spinner = '<div class="sk-circle"><div class="sk-circle1 sk-child"></div><div class="sk-circle2 sk-child"></div><div class="sk-circle3 sk-child"></div><div class="sk-circle4 sk-child"></div><div class="sk-circle5 sk-child"></div><div class="sk-circle6 sk-child"></div><div class="sk-circle7 sk-child"></div><div class="sk-circle8 sk-child"></div><div class="sk-circle9 sk-child"></div><div class="sk-circle10 sk-child"></div><div class="sk-circle11 sk-child"></div><div class="sk-circle12 sk-child"></div></div>';
