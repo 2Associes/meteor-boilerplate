3fc18d3843331f5e6eec9d5975f2dcf610345c2e
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:03:43 -0400

    Add paragraphs helper to body template

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,11 @@
 import { Template } from 'meteor/templating';
 
 import './body.html';
+
+Template.body.helpers({
+  paragraphs: [
+    { text: 'This is paragraph 1' },
+    { text: 'This is paragraph 2' },
+    { text: 'This is paragraph 3' },
+  ],
+});
