dc3e09dbc02b8f192573ab6253767cf4a8ea49a0
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:45:03 -0400

    Comment out original server's main.js

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
-import { Meteor } from 'meteor/meteor';
-
-import '../imports/api/paragraphs.js';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+// import { Meteor } from 'meteor/meteor';
+//
+// import '../imports/api/paragraphs.js';
+//
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
