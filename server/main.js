dd09823853101bd4b897ca101f8764c9c5f4063f
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:19:55 -0400

    Delete obsolete flemay:less-autoprefixer options

--- server/main.js
+++ server/main.js
@@ -2,9 +2,6 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/paragraphs.js';
 
-// Define flemay:less-autoprefixer options
-AUTOPREFIXER_OPTIONS = '{ "browsers": ["Chrome 36", "iOS 7"]}';
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
