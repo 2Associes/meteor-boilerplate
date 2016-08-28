533b77d13573a361a47ca38152e3ffa1623ebf9c
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:22:01 -0400

    Comment body static array

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,9 +5,10 @@ import { Paragraphs } from '../api/paragraphs.js';
 import './body.html';
 
 Template.body.helpers({
-  paragraphs: [
-    { text: 'This is paragraph 1' },
-    { text: 'This is paragraph 2' },
-    { text: 'This is paragraph 3' },
-  ],
+  // paragraphs: [
+  //   { text: 'This is paragraph 1' },
+  //   { text: 'This is paragraph 2' },
+  //   { text: 'This is paragraph 3' },
+  // ],
+  
 });
