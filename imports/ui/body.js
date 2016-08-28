7b8413bfa69b287c0e98cf1b867864786e4dec91
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:25:51 -0400

    Add comment in body helper

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,11 +5,14 @@ import { Paragraphs } from '../api/paragraphs.js';
 import './body.html';
 
 Template.body.helpers({
+  // Sample for static array
   // paragraphs: [
   //   { text: 'This is paragraph 1' },
   //   { text: 'This is paragraph 2' },
   //   { text: 'This is paragraph 3' },
   // ],
+
+  // Mongo Collection
   paragraphs() {
     return Paragraphs.find({});
   },
