a5538bd1e5d80af8b61b0bf8956270d9cc6587ff
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:50:10 -0400

    Update fixtures server's console message

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -5,6 +5,8 @@ import { Paragraphs } from '../../api/paragraphs.js';
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
   if (!Paragraphs.findOne()) {
-    console.log('No Paragraphs in the database... Create some sample data...');
+    console.log('No paragraph in the database... Create some sample paragraphs...');
+
+    
   }
 });
