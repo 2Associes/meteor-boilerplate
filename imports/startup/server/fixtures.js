c0e2a228a354f7224fe38854772d55f35d9ebe1c
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:33:55 -0400

    Add Meteor.startup function

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,3 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Paragraphs } from '../../api/paragraphs.js';
+
+// if the database is empty on server start, create some sample data.
+Meteor.startup(() => {
+  
+});
