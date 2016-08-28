864a48e992fbc2aa8b19d0eae029338d22fa5d8f
Hugues Brunelle <hugues@2associes.com>
2016-08-28 16:42:54 -0400

    Add server console message if database is empty

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -4,5 +4,7 @@ import { Paragraphs } from '../../api/paragraphs.js';
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
-  
+  if (!Paragraphs.findOne()) {
+    console.log('No data');
+  }
 });
