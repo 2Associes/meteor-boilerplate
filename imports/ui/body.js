0c9870f025857c2512cb95b3a2b7fbf14a6a36ca
Hugues Brunelle <hugues@2associes.com>
2016-08-29 12:41:15 -0400

    Move Meteor template from body.js to app.js

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,5 +1,3 @@
-import { Template } from 'meteor/templating';
-
 import { Paragraphs } from '../api/paragraphs.js';
 
 import './body.html';
--- imports/ui/layouts/app.js
+++ imports/ui/layouts/app.js
@@ -1,6 +1,7 @@
 import './app.html';
 
 import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
 
 Template.app.helpers({});
 
