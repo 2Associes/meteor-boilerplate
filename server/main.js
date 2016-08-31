f3c7e63840c0e047b3a334a3d8f849b6d152fd32
Hugues Brunelle <hugues@2associes.com>
2016-08-31 13:46:49 -0400

    Remove obsolete comments in server main.js

--- server/main.js
+++ server/main.js
@@ -1,9 +1,1 @@
-// import { Meteor } from 'meteor/meteor';
-//
-// import '../imports/api/paragraphs.js';
-//
-// Meteor.startup(() => {
-//   // code to run on server at startup
-// });
-
 import '/imports/startup/server';
