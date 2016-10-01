29e6316dffc90e01bfbb5886888c180453935108
Hugues Brunelle <hugues@2associes.com>
2016-10-01 17:03:46 -0400

    Add comments

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,8 @@
 // This defines a starting set of data to be loaded if the app is loaded with an empty db.
 import './fixtures.js';
 
+// Defines accounts services
 import './accounts.js';
 
+// Defines email setup
 import './email.js';
