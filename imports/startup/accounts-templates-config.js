744375263c8211cccb3f53ee22f7b0645e90f86b
Hugues Brunelle <hugues@2associes.com>
2016-10-16 10:34:03 -0400

    Add lowercaseUsername attribute

--- imports/startup/accounts-templates-config.js
+++ imports/startup/accounts-templates-config.js
@@ -9,9 +9,10 @@ AccountsTemplates.configure({
   showForgotPasswordLink: true,
   overrideLoginErrors: true,
   enablePasswordChange: true,
+  lowercaseUsername: true,
 
-  // sendVerificationEmail: true,
-  // enforceEmailVerification: true,
+  //sendVerificationEmail: true,
+  //enforceEmailVerification: true,
   //confirmPassword: true,
   //continuousValidation: false,
   //displayFormLabels: true,
