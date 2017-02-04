4d4b1b57b44e0d1d2ab9705567b8e06b4674c322
Hugues Brunelle <hugues@2associes.com>
2017-02-04 18:25:53 -0500

    Add footer component to client

---
+++ imports/ui/components/footer.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+
+import './footer.html';
+
+Template.footer.helpers({
+  meteorRelease() {
+    return Meteor.release;
+  }
+});
+
+Template.footer.events({});
