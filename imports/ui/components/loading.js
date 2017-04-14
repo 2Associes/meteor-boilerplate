b6d36a07dc81113b30209a5a056c784eb9f6a341
Hugues Brunelle <hugues@2associes.com>
2017-04-13 20:09:53 -0400

    [#78] revert eslint meteor/no-template-lifecycle-assignments

--- imports/ui/components/loading.js
+++ imports/ui/components/loading.js
@@ -7,7 +7,7 @@ import './loading.html';
 var message = '<p class="loading-message"></p>';
 var spinner = '<div class="sk-circle"><div class="sk-circle1 sk-child"></div><div class="sk-circle2 sk-child"></div><div class="sk-circle3 sk-child"></div><div class="sk-circle4 sk-child"></div><div class="sk-circle5 sk-child"></div><div class="sk-circle6 sk-child"></div><div class="sk-circle7 sk-child"></div><div class="sk-circle8 sk-child"></div><div class="sk-circle9 sk-child"></div><div class="sk-circle10 sk-child"></div><div class="sk-circle11 sk-child"></div><div class="sk-circle12 sk-child"></div></div>';
 
-Template.loading.onRendered = function () {
+Template.loading.rendered = function () {
   // launch splash
   this.loading = window.pleaseWait({
     logo: '/images/2associes-logo.png',
@@ -28,6 +28,6 @@ Template.loading.onRendered = function () {
   // }, 3000);
 };
 
-Template.loading.onDestroyed = function () {
+Template.loading.destroyed = function () {
   this.loading.finish();
 };
