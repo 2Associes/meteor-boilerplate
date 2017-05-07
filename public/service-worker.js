0cdb92939e68082e2e3bc52f5716566e7f4c0bc7
Hugues Brunelle <hugues@2associes.com>
2017-05-07 08:50:16 -0400

    [#105] ESlint service-workers.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,61 @@
 // configuration
-const
-  version = '1.0.0',
-  CACHE = version + '::PWAsite',
-  offlineURL = '/offline/',
-  installFilesEssential = [
-    '/',
-    '/manifest.webmanifest',
-  ].concat(offlineURL),
-  installFilesDesirable = [
-    '/images/2associes-logo.png',
-  ];
+
+// const
+//   version = '1.0.0',
+//   CACHE = version + '::PWAsite',
+//   offlineURL = '/offline/',
+//   installFilesEssential = [
+//     '/',
+//     '/manifest.webmanifest',
+//   ].concat(offlineURL),
+//   installFilesDesirable = [
+//     '/images/2associes-logo.png',
+//   ];
 
 // install static assets
-function installStaticFiles() {
-  return caches.open(CACHE)
-  .then(cache => {
-    // cache desirable files
-    cache.addAll(installFilesDesirable);
-    // cache essential files
-    return cache.addAll(installFilesEssential);
-  });
-}
+
+// function installStaticFiles() {
+//   return caches.open(CACHE)
+//   .then(cache => {
+//     // cache desirable files
+//     cache.addAll(installFilesDesirable);
+//     // cache essential files
+//     return cache.addAll(installFilesEssential);
+//   });
+// }
 
 // clear old caches
-function clearOldCaches() {
-  return caches.keys()
-  .then(keylist => {
-    return Promise.all(
-      keylist
-      .filter(key => key !== CACHE)
-      .map(key => caches
-      .delete(key),
-    ));
-  });
-}
+
+// function clearOldCaches() {
+//   return caches.keys()
+//   .then(keylist => {
+//     return Promise.all(
+//       keylist
+//       .filter(key => key !== CACHE)
+//       .map(key => caches
+//       .delete(key),
+//     ));
+//   });
+// }
 
 // application installation
-self.addEventListener('install', event => {
+
+self.addEventListener('install', () => {
   console.log('service worker: install');
   // cache core files
-  event.waitUntil(
-    installStaticFiles()
-    .then(() => self.skipWaiting()),
-  );
+  // event.waitUntil(
+  //   installStaticFiles()
+  //   .then(() => self.skipWaiting()),
+  // );
 });
 
 // application activated
-self.addEventListener('activate', event => {
+
+self.addEventListener('activate', () => {
   console.log('service worker: activate');
   // delete old caches
-  event.waitUntil(
-    clearOldCaches()
-    .then(() => self.clients.claim()),
-  );
+  // event.waitUntil(
+  //   clearOldCaches()
+  //   .then(() => self.clients.claim()),
+  // );
 });
