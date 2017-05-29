a467b11026ff47e5793e72c82995c803823b7409
Hugues Brunelle <hugues@2associes.com>
2017-05-29 08:25:15 -0400

    [#49] Replace paragraphs api for visitors

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,75 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 
-import { Paragraphs } from '../../api/paragraphs';
+import { Visitors } from '../../api/visitors';
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
-  if (!Paragraphs.findOne()) {
+  if (!Visitors.findOne()) {
     // No paragraph in the database... Create some sample paragraphs...
-    let timestamp = new Date().getTime();
+    const timestamp = new Date().getTime();
 
-    _.each(_.range(3), () => {
-      const randomText = faker.lorem.paragraph();
-      const randomI18nEnText = faker.lorem.paragraph();
-
-      Paragraphs.insert({
-        text: randomText,
-        i18n: {
-          en: {
-            text: randomI18nEnText,
-          },
-        },
-        createdAt: new Date(timestamp),
-      });
-
-      timestamp += 1000; // ensure unique timestamp.
-    });
-  }
-
-  if (!Meteor.users.findOne()) {
-    // No user in the database... Create some sample users with roles...
-    const users = [
-      {
-        name: 'Normal User',
-        username: 'normaluser',
-        email: 'normal@example.com',
-        roles: [],
-      },
-      {
-        name: 'View-Secrets User',
-        username: 'viewsecretsuser',
-        email: 'view@example.com',
-        roles: ['view-secrets'],
-      },
-      {
-        name: 'Manage-Users User',
-        username: 'manageusersuser',
-        email: 'manage@example.com',
-        roles: ['manage-users'],
-      },
-      {
-        name: 'Admin User',
-        username: 'adminuser',
-        email: 'admin@example.com',
-        roles: ['admin'],
-      },
-    ];
-
-    _.each(users, (user) => {
-      const id = Accounts.createUser({
-        email: user.email,
-        password: '123456',
-        username: user.username,
-        profile: {
-          name: user.name,
-        },
-      });
-
-      if (user.roles.length > 0) {
-        // Need _id of existing user record so this call must come
-        // after `Accounts.createUser` or `Accounts.onCreate`
-        Roles.addUsersToRoles(id, user.roles, 'default-group');
-      }
+    Visitors.insert({
+      from: 'Rive-Sud',
+      heardFrom: 'Autre',
+      createdAt: new Date(timestamp),
     });
   }
 });
