8311a8fd82c47b2553c256d1f5f9e7f3abd46382
Hugues Brunelle <hugues@2associes.com>
2017-05-29 08:25:55 -0400

    [#49] Update home

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,24 +1,58 @@
-import { Paragraphs } from '../../api/paragraphs';
-
 import './home.html';
 
 Template.home.onCreated(() => {
-  // Subscriptions
-  TAPi18n.subscribe('paragraphs');
+  Session.set('whereAreYouFrom', '');
+  Session.set('howDidYouHeardOf', '');
 });
 
-Template.home.helpers({
-  // Sample for static array
-  // paragraphs: [
-  //   { text: 'This is paragraph 1' },
-  //   { text: 'This is paragraph 2' },
-  //   { text: 'This is paragraph 3' },
-  // ],
+Template.home.helpers({});
 
-  // Mongo Collection
-  paragraphs() {
-    return Paragraphs.find();
+Template.home.events({
+  'click .where button'(event) {
+    if ($(event.target).attr('class') === 'btn-primary') {
+      $(event.target).removeClass('btn-primary');
+      Session.set('whereAreYouFrom', '');
+    } else {
+      $('.where button').removeClass('btn-primary');
+      Session.set('whereAreYouFrom', event.currentTarget.textContent);
+      $(event.target).addClass('btn-primary');
+    }
+  },
+
+  'click .how button'(event) {
+    if ($(event.target).attr('class') === 'btn-primary') {
+      $(event.target).removeClass('btn-primary');
+      Session.set('howDidYouHeardOf', '');
+    } else {
+      $('.how button').removeClass('btn-primary');
+      Session.set('howDidYouHeardOf', event.currentTarget.textContent);
+      $(event.target).addClass('btn-primary');
+    }
+  },
+
+  'submit form'(event, template) {
+    event.preventDefault();
+
+    const visitorFrom = Session.get('whereAreYouFrom');
+    const visitorHeardFrom = Session.get('howDidYouHeardOf');
+
+    if ( visitorFrom === '' || visitorHeardFrom === '' ) {
+      alert('SVP répondre aux 2 questions.');
+    } else {
+      // Insert a visitor into the collection
+      Meteor.call('insertVisitor', visitorFrom, visitorHeardFrom, function(error, result) {
+
+        if (!error) {
+          $('input').attr('disabled','disabled');
+          // Clear form
+          $('button').removeClass('btn-primary');
+          $('input').removeAttr('disabled');
+          Session.set('whereAreYouFrom', '');
+          Session.set('howDidYouHeardOf', '');
+        } else {
+          alert('Une erreur est survenue lors de l\'enregistrement! SVP rafraîchir la page et recommencer.');
+        }
+      });
+    }
   },
 });
-
-Template.home.events({});
