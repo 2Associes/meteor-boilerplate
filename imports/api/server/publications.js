58704313088ec8253dfcb1fa95029b31248b6dff
Hugues Brunelle <hugues@2associes.com>
2017-06-23 15:29:03 -0400

    [#92] Add paragraphsPublication name to function

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -1,5 +1,5 @@
 import { Paragraphs } from '../paragraphs';
 
-TAPi18n.publish('paragraphs', function () {
+TAPi18n.publish('paragraphs', function paragraphsPublication() {
   return Paragraphs.i18nFind();
 });
