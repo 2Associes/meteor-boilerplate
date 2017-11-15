bafe96894b57e3981c9c0e899e07693c48424a90
Simon Martineau <marsimo65@gmail.com>
2017-11-15 15:22:32 -0500

    [#157] Add global validation file

---
+++ imports/api/validation.js
@@ -0,0 +1,19 @@
+import { Meteor } from 'meteor/meteor'
+
+const keys = {
+  'authors.name'(value) {
+    if (!value) {
+      throw new Meteor.Error('empty', 'Name is empty')
+    }
+  }
+}
+
+export default {
+  validate(key, ...args) {
+    if (typeof key === 'object') {
+      Object.keys(key).forEach((singleKey) => {
+        this.validate(singleKey, key[singleKey], ...args)
+      })
+    } else if (typeof keys[key] === 'function') (keys[key])(...args)
+  }
+}
