cec166ab5d2378cd6a9eba2fe2644d613f43303c
Hugues Brunelle <hugues@2associes.com>
2018-01-24 17:04:18 -0500

    [#187] Update service worker check function

--- client/main.js
+++ client/main.js
@@ -13,10 +13,14 @@ Meteor.startup(() => {
   $('html').attr('lang', getUserLanguage())
 })
 
-// enable service worker
-if ('serviceWorker' in navigator) {
-  // register service worker
-  navigator.serviceWorker.register('/service-worker.js', {
+// Check for registered service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA] Active service worker found, no need to register')
+} else {
+  // Register the ServiceWorker
+  navigator.serviceWorker.register('service-worker.js', {
     scope: './'
+  }).then(function (reg) {
+    console.log(`[PWA] Service worker has been registered for scope: ${reg.scope}`)
   })
 }
