9feb2f068fe1e19dedd4a3bfc1a1c91862f7a182
Andres Flores <andres@2associes.com>
2018-03-14 16:19:43 -0400

    [#185] Use default instance for testing purposes

--- imports/startup/client/addToHomeScreen.js
+++ imports/startup/client/addToHomeScreen.js
@@ -1,11 +1,5 @@
-import { TAPi18n } from 'meteor/tap:i18n'
-
 Meteor.startup(() => {
-  Tracker.autorun(() => {
-    AddToHomeScreen.init({
-      close: 'Something else',
-      text: TAPi18n.__('addToHomeScreen.bodyText'),
-      recurrences: 999
-    })
+  AddToHomeScreen.init({
+    recurrences: 999
   })
 })
